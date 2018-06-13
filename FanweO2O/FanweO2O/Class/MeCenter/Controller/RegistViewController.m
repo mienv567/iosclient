5c4d8aa1e0758253d357e82933b9196abfc6ceab
xander <xander1@gmail.com>
2018-06-13 22:11:49 +0800

    新加控制器

---
+++ FanweO2O/FanweO2O/Class/MeCenter/Controller/RegistViewController.m
@@ -0,0 +1,37 @@
+//
+//  RegistViewController.m
+//  ZhoubaitongO2O
+//
+//  Created by Harlan on 2018/6/13.
+//  Copyright © 2018年 xfg. All rights reserved.
+//
+
+#import "RegistViewController.h"
+
+@interface RegistViewController ()
+
+@end
+
+@implementation RegistViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

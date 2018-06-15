c020a165987a2a89349835fede230efb258cfa51
xander <xander1@gmail.com>
2018-06-15 21:21:51 +0800

    ...新加  未登录

---
+++ FanweO2O/FanweO2O/Class/MeCenter/Controller/MyCenterViewController1.h
@@ -0,0 +1,13 @@
+//
+//  MyCenterViewController1.h
+//  ZhoubaitongO2O
+//
+//  Created by Harlan on 2018/6/15.
+//  Copyright © 2018年 xfg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyCenterViewController1 : UIViewController
+
+@end
---
+++ FanweO2O/FanweO2O/Class/MeCenter/Controller/MyCenterViewController1.m
@@ -0,0 +1,37 @@
+//
+//  MyCenterViewController1.m
+//  ZhoubaitongO2O
+//
+//  Created by Harlan on 2018/6/15.
+//  Copyright © 2018年 xfg. All rights reserved.
+//
+
+#import "MyCenterViewController1.h"
+
+@interface MyCenterViewController1 ()
+
+@end
+
+@implementation MyCenterViewController1
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

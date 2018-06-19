a4fd8a061f9fe2ce67eb0e619c5f14ba2634a518
xander <xander1@gmail.com>
2018-06-19 21:23:33 +0800

    修改细节

--- FanweO2O/FanweO2O/Class/Home/searchController/SearchViewController.m
+++ FanweO2O/FanweO2O/Class/Home/searchController/SearchViewController.m
@@ -19,7 +19,6 @@
     
     UITextField *TF = [[UITextField alloc] init];
     TF.frame = CGRectMake(0, 0, ScreenW - (130 *kScreenWidthRatio), 30);
-    TF.placeholder = @"搜索店铺";
     TF.layer.masksToBounds =YES;
     TF.layer.cornerRadius = CGRectGetHeight(TF.frame) / 2;
     TF.backgroundColor = [UIColor colorWithRed:255 green:255 blue:255 alpha:1.00];
@@ -32,6 +31,9 @@
     [view addSubview:lb];
     TF.leftView = view;
     TF.delegate = self;
+    NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
+    attributes[NSForegroundColorAttributeName] = [UIColor grayColor];
+    TF.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"搜索店铺" attributes:attributes];
     TF.keyboardType = UIReturnKeySearch;
     TF.font = [UIFont systemFontOfSize:13.0];
     TF.clearButtonMode = UITextFieldViewModeWhileEditing;
@@ -43,6 +45,8 @@
     NSLog(@"点击了搜索");
     return YES;
 }
-
+- (void)viewWillDisappear:(BOOL)animated {
+    [self.view endEditing:YES];
+}
 
 @end

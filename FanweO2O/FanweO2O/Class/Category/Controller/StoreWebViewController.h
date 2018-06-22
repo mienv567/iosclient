af89cc631d40c52569cbe87ab2b31051d2de75ce
Harlan <harlan123@gmail.com>
2018-06-22 21:31:59 +0800

    分类跳转到商家列表

---
+++ FanweO2O/FanweO2O/Class/Category/Controller/StoreWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  StoreWebViewController.h
+//  ZhoubaitongO2O
+//
+//  Created by harlan on 2018/6/22.
+//  Copyright © 2018年 xfg. All rights reserved.
+//
+
+#import "BaseWebController.h"
+
+@interface StoreWebViewController : BaseWebController
+
+@end
---
+++ FanweO2O/FanweO2O/Class/Category/Controller/StoreWebViewController.m
@@ -0,0 +1,26 @@
+//
+//  StoreWebViewController.m
+//  ZhoubaitongO2O
+//
+//  Created by harlan on 2018/6/22.
+//  Copyright © 2018年 xfg. All rights reserved.
+//
+
+#import "StoreWebViewController.h"
+
+@interface StoreWebViewController ()
+
+@end
+
+@implementation StoreWebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+
+
+@end
